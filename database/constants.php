b0f558658e328a8e5e0474f9a9d132d2402d9e66
Amir Alam <sharpprogrammer2018@gmail.com>
2022-01-08 10:52:07 -0800

    added db props

--- database/constants.php
+++ database/constants.php
@@ -1,8 +1,8 @@
 <?php
     session_start();
     define("HOST", "huygens.in");
-    define("USER","root");
-    define("PASS","");
-    define("DB","huygens");
+    define("USER","u481765919_admin");
+    define("PASS","Huygens1194@#");
+    define("DB","u481765919_huygens");
     define("DOMAIN","http://huygens.in/");
 ?>
