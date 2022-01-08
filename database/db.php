8e7ab71b4663674f524f51b2159b57a3592e83cc
Amir Alam <sharpprogrammer2018@gmail.com>
2022-01-08 10:58:22 -0800

    s

--- database/db.php
+++ database/db.php
@@ -7,6 +7,7 @@
             include_once("constants.php");
             $this->con = new Mysqli(HOST,USER,PASS,DB);
             if ($this->con) {
+                echo "Connection succesfull";
                 return $this->con;
             }
             return "Database connection failed";
